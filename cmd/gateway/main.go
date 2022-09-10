47a39659dbf3a9a53c5cd4afa017df19f85c5ec0
Daniel Sarmiento <daniel.sarmiento3@dafiti.com.co>
2022-09-10 04:35:17 -0500

    changes and commit

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -24,8 +24,63 @@ var microservices = []types.Microservice{
 		Name: "posts",
 		Url:  "http://bird:8050/api/v1/",
 	},
+	{
+		Name: "finance",
+		Url:  "http://finance:8086/api/v1/",
+	},
 }
 
+func requestController(c *gin.Context) {
+
+	authorization := strings.Split(c.Request.Header["Authorization"][0], " ")[1]
+	accessToken := strings.ReplaceAll(authorization, "Bearer ", "")
+
+	token, err := jwt.ParseWithClaims(accessToken, &types.UserClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(configs.Get("JWT_KEY")), nil
+	})
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Invalid authorization",
+			"data":    err.Error(),
+		})
+		return
+	}
+	user := token.Claims.(*types.UserClaims)
+
+	name := c.Param("microservice")
+	method := c.Param("method")
+	var microservice types.Microservice
+	for _, m := range microservices {
+		if m.Name == name {
+			microservice = m
+		}
+	}
+
+	bodyAsByteArray, _ := ioutil.ReadAll(c.Request.Body)
+	jsonBody := string(bodyAsByteArray)
+
+	payload := map[string]interface{}{"payload": jsonBody, "user": user}
+
+	byts, _ := json.Marshal(payload)
+	url := microservice.Url + method
+
+	req, _ := http.NewRequest(c.Request.Method, url, bytes.NewBuffer(byts))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "error",
+			"data":    err.Error(),
+		})
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	jsonBodyResponse := string(body)
+	c.JSON(http.StatusOK, gin.H{
+		"result": jsonBodyResponse,
+	})
+}
 func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
@@ -36,52 +91,10 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 	r.POST(":microservice/:method", func(c *gin.Context) {
-		authorization := strings.Split(c.Request.Header["Authorization"][0], " ")[1]
-		accessToken := strings.ReplaceAll(authorization, "Bearer ", "")
-
-		token, err := jwt.ParseWithClaims(accessToken, &types.UserClaims{}, func(token *jwt.Token) (interface{}, error) {
-			return []byte(configs.Get("JWT_KEY")), nil
-		})
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Invalid authorization",
-				"data":    err.Error(),
-			})
-			return
-		}
-		user := token.Claims.(*types.UserClaims)
-
-		name := c.Param("microservice")
-		method := c.Param("method")
-		var microservice types.Microservice
-		for _, m := range microservices {
-			if m.Name == name {
-				microservice = m
-			}
-		}
-
-		bodyAsByteArray, _ := ioutil.ReadAll(c.Request.Body)
-		jsonBody := string(bodyAsByteArray)
-
-		payload := map[string]interface{}{"payload": jsonBody, "user": user}
-		byts, _ := json.Marshal(payload)
-		url := microservice.Url + method
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(byts))
-		req.Header.Set("Content-Type", "application/json")
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "error",
-				"data":    err.Error(),
-			})
-		}
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		jsonBodyResponse := string(body)
-		c.JSON(http.StatusOK, gin.H{
-			"result": jsonBodyResponse,
-		})
+		requestController(c)
+	})
+	r.GET(":microservice/:method", func(c *gin.Context) {
+		requestController(c)
 	})
 	r.Run()
 }
